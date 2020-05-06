28afd8a247930e2d794d619cbc738ac1e5a51959
rajabi2001 <mj.rajabi2001@gmail.com>
2020-05-06 22:23:29 +0430

    prime changes

--- game_manager.py
+++ game_manager.py
@@ -6,16 +6,62 @@ from cell import Cell
 class GameManager:
 
     def __init__(self, size, screen, sx, sy, block_cells):
-        pass
+        self.screen = screen
+        self.size = size
+        self.cells = []
+        self.sx = sx
+        self.sy = sy
+        self.snakes = list()
+        self.turn = 0
+        for i in range(self.size):
+            tmp = []
+            for j in range(self.size):
+                tmp.append(Cell(screen, sx + i * consts.cell_size, sy + j * consts.cell_size))
+            self.cells.append(tmp)
+        for cell in block_cells:
+            self.get_cell(cell).set_color(consts.block_color)
 
     def add_snake(self, snake):
-        pass
+        self.snakes.append(snake)
 
     def get_cell(self, pos):
-        pass
+        try:
+            return self.cells[pos[0]][pos[1]]
+        except:
+            return None
 
     def kill(self, killed_snake):
-        pass
+        self.snakes.remove(killed_snake)
+
+    def get_next_fruit_pos(self): # returns tuple (x, y) that is the fruit location
+        ret = -1, -1
+        mx = -100
+
+        for i in range(0, self.size):
+            for j in range(0, self.size):
+
+                mn = 100000000
+
+                for x in range(0, self.size):
+                    for y in range(0, self.size):
+                        if self.get_cell((x, y)).color != consts.back_color:
+                            mn = min(mn, int( abs(x-i) + abs(y-j) ))
+
+
+                if mn > mx:
+                    mx = mn
+                    ret = i, j
+
+            yield ret
+
 
     def handle(self, keys):
-        pass
+        for snake in self.snakes:
+            snake.handle(keys)
+            snake.next_move()
+            self.turn +=1
+            if self.turn % 10 == 0:
+                fruit_pos = list(self.get_next_fruit_pos())
+                fruit_pos.sort()
+                self.get_cell(fruit_pos[0]).set_color(consts.fruit_color)
+            
